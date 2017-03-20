5110018e562e57d962cfe6dba6472986811c6c36
Seven <shiweidu@outlook.com>
2017-03-20 13:37:52 +0800

    使用 fans特有主体配色

--- src/App.js
+++ src/App.js
@@ -2,12 +2,9 @@ import React, { Component } from 'react';
 import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import lightBaseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
-import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import Main from './containers/Main';
 import store from './createStore';
-
-const theme = getMuiTheme(lightBaseTheme);
+import theme from './styles//fansTheme';
 
 const App = () => (
   <Provider store={store}>
